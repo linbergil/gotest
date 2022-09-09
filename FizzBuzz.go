be0ddfcb3246a056e37a3291ab93ee566c6ac911
Ilya Smyshlyaev <smyshlyaev.il@yandex.ru>
2022-09-09 14:37:49 +0300

    Change if-else on switch-case

--- FizzBuzz.go
+++ FizzBuzz.go
@@ -14,4 +14,22 @@ func main() {
 			fmt.Println(i)
 		}
 	}
+
+	switchFB()
+}
+
+func switchFB() {
+	for i := 1; i < 101; i++ {
+		switch {
+		case i%15 == 0:
+			fmt.Println("FizzBuzz")
+		case i%3 == 0:
+			fmt.Println("Fizz")
+		case i%5 == 0:
+			fmt.Println("Buzz")
+		default:
+			fmt.Println(i)
+		}
+	}
+
 }
