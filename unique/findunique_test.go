376ff4255a38d68e2cf1349b99f89e08d55ee3fc
Ilya Smyshlyaev <smyshlyaev.il@yandex.ru>
2022-09-19 09:36:01 +0300

    some changes in unique and unique test

--- unique/findunique_test.go
+++ unique/findunique_test.go
@@ -14,32 +14,56 @@ type testdata struct {
 	output int
 }
 
-var test = []testdata{
+var odd_test = []testdata{
 	{[]int{9, 3, 9, 3, 9, 7, 9}, 7},
-	{[]int{9, 3, 9, 8, 9, 7, 9}, 3},
+	{[]int{9, 3, 9, 8, 9, 3, 9}, 8},
 }
 
-func TestRotation(t *testing.T) {
+var even_test = []testdata{
+	{[]int{9, 3, 9, 3, 9, 7}, 0},
+}
+
+// сравниваем входные данные с выходными данными, и кидаем ошибки или пишем в терминал
+func test(d *testdata, t *testing.T) {
+
+	result := unique.FindUnique(d.input)
+
+	if result != d.output {
+		t.Error(
+			"test failed",
+			"for: ", d.input,
+			"expected: ", d.output,
+			"got: ", result,
+		)
+	} else {
+		fmt.Println(
+			"test passed",
+			"for: ", d.input,
+			"got: ", result,
+		)
+	}
+}
+
+func TestUnique(t *testing.T) {
 
 	t.Run(
-		"insert nums", func(t *testing.T) {
+		"inserting odd nums of elements", func(t *testing.T) {
 
-			for _, data := range test {
+			for _, data := range odd_test {
 
-				result := unique.FindUnique(data.input)
+				test(&data, &testing.T{})
 
-				if result != data.output {
-					t.Error(
-						"For: ", data.input,
-						"Expected: ", data.output,
-						"Got: ", result,
-					)
-				} else {
-					fmt.Println(
-						"For: ", data.input,
-						"Got: ", result,
-					)
-				}
+			}
+		},
+	)
+
+	t.Run(
+		"inserting even nums of elements", func(t *testing.T) {
+
+			for _, data := range even_test {
+
+				test(&data, &testing.T{})
+
 			}
 		},
 	)
